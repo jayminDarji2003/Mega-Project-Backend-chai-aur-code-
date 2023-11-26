5e1bab8c0105f993a23d985dbafb4b2cc0dc06c0
Jaymin Darji <jaymindarji2003@gmail.com>
2023-11-26 12:16:16 +0530

    learning backend

--- src/app.js
+++ src/app.js
@@ -9,11 +9,25 @@ app.use(cors({
     credentials: true,
 }))
 
+// ------------- configuration start here --------------------
+
 // here we are accepting json formate data.
 app.use(express.json({
     limit: "16kb",
 }))
 
 // here we are accepting data from url. 
+app.use(express.urlencoded({
+    extended: true,
+    limit: "16kb"
+}))
+
+//keep some images, pdfs, documents 
+app.use(express.static("public"))
+
+// for cookies
+app.use(cookieParser)
+
+// ------------- configuration ends here --------------------
 
 export { app };
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,10 @@
+// const asyncHandler = () => { }
+
+export { asyncHandler }
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () {}
+// const asyncHandler = (func) => async () => () {}
+
+const asyncHandler = 
