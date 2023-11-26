401a19eab6a361036956fdfd1390d230e82bab1c
Jaymin Darji <jaymindarji2003@gmail.com>
2023-11-26 16:51:21 +0530

    last commit because plan changed

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,8 @@
-// const asyncHandler = () => { }
+const asyncHandler = (reqestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(reqestHandler(req, res, next)).catch((error) => next(error))
+    }
+}
 
 export { asyncHandler }
 
@@ -7,4 +11,15 @@ export { asyncHandler }
 // const asyncHandler = (func) => () {}
 // const asyncHandler = (func) => async () => () {}
 
-const asyncHandler = 
+// using try catch
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
